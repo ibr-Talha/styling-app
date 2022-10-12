478d80530739621f063e93e8feac234f78fc2015
talha <talha@gmail.com>
2022-10-12 22:18:51 +0300

    useStateCounter

--- src/compenent/3-useStateHook/UseStateCounter.jsx
+++ src/compenent/3-useStateHook/UseStateCounter.jsx
@@ -21,7 +21,7 @@
 const UseStateCounter = () => {
   return (
       <div className="container text-center mt-4">
-        <h1>CLASSFUL COMPONENTS</h1>
+        <h1>USESTATE HOOK</h1>
         <h2 className="display-4 text-danger">COUNT:</h2>
         <button className="btn btn-success">INC</button>
         <button className="btn btn-danger">DEC</button>
