7406c1dedc6e4ac52c718303c78cdb6266d4d31a
talha <talha@gmail.com>
2022-10-12 20:42:23 +0300

    buton-eventDefinition

--- src/compenent/1-event/Events.jsx
+++ src/compenent/1-event/Events.jsx
@@ -17,16 +17,25 @@ const Events = () => {
   const handleClear = (msg) => {
     alert(msg);
   };
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
   return (
     <div className="container text-center mt-4">
       <button onClick={handleClick} className="btn btn-success">
         Click
       </button>
       <button
-        onClick={handleClear("Clear Btn Clicked")}
+        onClick={() => handleClear("Clear Btn Clicked")}
         className="btn btn-dark">
         Clear
       </button>
+      <button onClick={handleChange} className="btn btn-danger">
+        Change
+      </button>
+      {/* <button onClick={(e) => handleChange} className="btn btn-danger">
+        Change
+      </button> */}
     </div>
   );
 };
