962eeb38aaf4d02f64469424dbb24344bca6f4f3
talha <talha@gmail.com>
2022-10-12 20:32:01 +0300

    onClick-btn

--- src/compenent/1-event/Events.jsx
+++ src/compenent/1-event/Events.jsx
@@ -11,9 +11,24 @@
 //? Ayrinti icin : https://reactjs.org/docs/events.html
 
 const Events = () => {
-  return <div>
-    <button>Click</button>
-  </div>;
+  const handleClick = () => {
+    alert("Btn Clicked");
+  };
+  const handleClear = (msg) => {
+    alert(msg);
+  };
+  return (
+    <div className="container text-center mt-4">
+      <button onClick={handleClick} className="btn btn-success">
+        Click
+      </button>
+      <button
+        onClick={handleClear("Clear Btn Clicked")}
+        className="btn btn-dark">
+        Clear
+      </button>
+    </div>
+  );
 };
 
 export default Events;
